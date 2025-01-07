c3a9c94f0cd3946792810b18db55d9d363037b72
Islam <developer.islamsaleh@gmail.com>
2025-01-07 21:08:15 +0200

    Solve 'Opposite number' (Tue, 7 January 2025)

---
+++ problems-solving-codewars/JavaScript/8 kyu/007-Opposite number.js
@@ -0,0 +1,16 @@
+/*
+ Task:
+		- Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+ Examples:
+		- 1: -1
+				14: -14
+				-34: 34
+
+ Tags:
+		- Fundamentals.
+*/
+
+function opposite(number) {
+ return -number;
+}
